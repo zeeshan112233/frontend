eed94cf27f37b3807a6be378e99ec2e780d4b39a
Importext <zeeshanyasin340@gmail.com>
2020-07-02 06:29:24 +0500

    spa part 1

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -10,12 +10,21 @@ import Home from "./HomeComponent";
 
 import { Switch, Route, Redirect } from "react-router-dom";
 
+import Contact from "./ContactComponent";
+
+import { COMMENTS } from "../shared/comments";
+import { PROMOTIONS } from "../shared/promotions";
+import { LEADERS } from "../shared/leaders";
+
 class Main extends Component {
   constructor(props) {
     super(props);
+
     this.state = {
       dishes: DISHES,
-      //   selectedDish: null,
+      comments: COMMENTS,
+      promotions: PROMOTIONS,
+      leaders: LEADERS,
     };
   }
 
@@ -25,12 +34,16 @@ class Main extends Component {
   //   }
 
   render() {
-    // const HomePage = () => {
-    //     return(
-    //         <Home
-    //         />
-    //     );
-    //   }
+    const HomePage = () => {
+      return (
+        <Home
+          dish={this.state.dishes.filter((dish) => dish.featured)[0]}
+          promotion={this.state.promotions.filter((promo) => promo.featured)[0]}
+          leader={this.state.leaders.filter((leader) => leader.featured)[0]}
+        />
+      );
+    };
+
     return (
       <div>
         <Header />
@@ -48,12 +61,15 @@ class Main extends Component {
         /> */}
 
         <Switch>
-          <Route path='/home' component={Home} />
+          <Route path='/home' component={HomePage} />
           <Route
             exact
             path='/menu'
             component={() => <Menu dishes={this.state.dishes} />}
           />
+
+          <Route exact path='/contactus' component={Contact} />
+
           <Redirect to='/home' />
         </Switch>
 
--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -3,7 +3,9 @@ import { Card, CardImg, CardImgOverlay, CardTitle } from "reactstrap";
 
 function RenderMenuItem({ dish, onClick }) {
   return (
-    <Card onClick={() => onClick(dish.id)}>
+    <Card 
+    // onClick={() => onClick(dish.id)}
+    >
       <CardImg width='100%' src={dish.image} alt={dish.name} />
       <CardImgOverlay>
         <CardTitle>{dish.name}</CardTitle>
