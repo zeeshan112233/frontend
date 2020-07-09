3410a4435d36f2c40a46c9c8d117f919c6e9c142
Importext <zeeshanyasin340@gmail.com>
2020-07-10 04:38:13 +0500

    cleaned code

--- src/pages/SignUpForm.js
+++ src/pages/SignUpForm.js
@@ -9,7 +9,6 @@ class SignUpForm extends Component {
     super();
 
     this.state = {
-      // email: "",
       password: "",
       name: "",
       hasAgreed: false,
@@ -33,68 +32,24 @@ class SignUpForm extends Component {
 
   uploadimage(e) {
     this.setState({ profilephoto: e.target.files[0] });
-
-    //   const param = e.target.files[0];
-    //   let reader = new FileReader();
-    //   reader.readAsDataURL(param);
-    //   this.setState({
-    //     profilephoto: reader,
-    //   });
-    //   console.log(reader);
   }
 
   handleSubmit(e) {
     e.preventDefault();
-
-    // let username = this.state.name;
-
-    // let password = this.state.password;
-
-    // let email = this.state.email;
-    //    let profilephoto = this.state.profilephoto.result;
-
-    // let data = {
-    //   username,
-    //   password,
-    //   // email,
-    //   //    profilephoto,
-    // };
-    //console.log(profilephoto);
-    /// we use this. set state method to reload a page  on changing state of a variabel
-
-    // this.setState({
-    //   signedup: true,
-    // });
-
-    // console.log(this.state.signedup);
 
     const formData = new FormData();
     formData.append("username", this.state.name);
     formData.append("password", this.state.password);
     formData.append("profilephoto", this.state.profilephoto);
 
-    // fetch(API, {
-    //   method: "POST",
-    //   // body: JSON.stringify(data),
-    //   body: JSON.stringify(formData),
-
-    //   headers: {
-    //     "Content-Type": "application/json",
-    //   },
-    // }).then(function (response) {
-    //   if (response.ok) {
-    //     alert("Signup Successfully done Bingo !!! ");
-    //   } else {
-    //     var error = new Error(response);
-    //     error.response = response;
-    //     throw error;
-    //   }
-    // });
-
     axios
       .post("http://localhost:3000/users/signup", formData, {})
       .then((res) => {
         console.log(res);
+
+        this.setState({
+          signedup: true,
+        });
       });
   }
 
@@ -105,26 +60,7 @@ class SignUpForm extends Component {
     } else {
       return (
         <div className='FormCenter'>
-          <form
-            onSubmit={this.handleSubmit}
-            // encType='multipart/form-data'
-            className='FormFields'
-          >
-            {/* <div className='FormField'>
-              <label className='FormField__Label' htmlFor='email'>
-                Email ID
-              </label>
-              <input
-                type='text'
-                id='email'
-                className='FormField__Input'
-                placeholder='Enter your email ID'
-                name='email'
-                value={this.state.email}
-                onChange={this.handleChange}
-              />
-            </div>
- */}
+          <form onSubmit={this.handleSubmit} className='FormFields'>
             <div className='FormField'>
               <label className='FormField__Label' htmlFor='name'>
                 Full Name
@@ -163,7 +99,6 @@ class SignUpForm extends Component {
                 type='file'
                 accept='image/*'
                 className='FormField__Input'
-                // value={this.state.profilephoto}
                 onChange={this.uploadimage}
               />
             </div>
